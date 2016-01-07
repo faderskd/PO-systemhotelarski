31017909c1a0c8f8208574cad629eea96008d0df
Bartłomiej Bęczkowski <bartlomiej.beczkowski@jmr.pl>
2016-01-07 17:43:38 +0100

    rozbudowany model uzytkownika, formularz rejestracyjny, migracje

--- systemhotelarski_front/systemhotelarski_front/users/forms.py
+++ systemhotelarski_front/systemhotelarski_front/users/forms.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
@@ -14,8 +16,20 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = get_user_model()
-        fields = ('email', 'username',)
+        fields = ('email', 'username', 'first_name', 'last_name',
+                  'date_of_birth')
         widgets = {
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'date_of_birth': forms.TextInput(attrs={'class': 'form-control',
+                                                    'placeholder': 'YYYY-MM-DD'}),
         }
+
+    def clean_date_of_birth(self):
+        date = self.cleaned_data['date_of_birth']
+        current_date = datetime.datetime.now().date()
+        if date > current_date:
+            raise forms.ValidationError('NOPE')
+        return date
