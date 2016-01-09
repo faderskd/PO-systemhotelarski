4e97bab1d59fc5b84fbc259fb3138e338ae8804e
daniel.faderski <daniel.faderski@jmr.pl>
2016-01-09 15:50:33 +0100

    rooms list view

--- systemhotelarski_backoffice/systemhotelarski/reservations/views.py
+++ systemhotelarski_backoffice/systemhotelarski/reservations/views.py
@@ -1,12 +1,15 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Room, Reservation
 from .serializers import RoomSerializer, ReservationSerializer
+from core.utlis import JSONResponse
 
 
-class RoomViewSet(viewsets.ModelViewSet):
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
+def room_list(request):
+    rooms = Room.objects.all()
+    room_serializer = RoomSerializer(rooms, many=True)
+    return JSONResponse(room_serializer.data)
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
--- systemhotelarski_backoffice/systemhotelarski/urls.py
+++ systemhotelarski_backoffice/systemhotelarski/urls.py
@@ -6,16 +6,12 @@ from rest_framework.routers import DefaultRouter
 
 
 router = DefaultRouter()
+router.register(r'rooms', views.RoomViewSet)
 router.register(r'reservations', views.ReservationViewSet)
-
-rooms_patterns = [
-    url(r'^$', views.room_list),
-]
-
 
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^rooms$', include(rooms_patterns, namespace='rooms')),
+
 ]
